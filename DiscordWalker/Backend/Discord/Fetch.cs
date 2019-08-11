21c3015355b3734fc935ceb86c7be390cf79d9fe
Oscar Davies <oscar.davies@hotmail.co.uk>
2019-08-11 16:33:47 +0100

    Able to Successfully Walk through Discords

---
+++ DiscordWalker/Backend/Discord/Fetch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace DiscordWalker.Backend.Discord
+{
+    public static class Fetch
+    {
+        //public static List<string> JoinedGuilds()
+        //{
+
+        //}
+
+        public static List<string> TextChannels(string GuildID)
+        {
+            List<string> Rooms = new List<string> { };
+            JToken Res = NetworkInterface.Request("guilds/" + GuildID + "/channels", WToken: true, Method: "GET");
+            if (Res != null)
+            {
+                foreach (JToken Channel in Res.Where(x => x["type"].ToString() == "0")) { Rooms.Add(Channel["id"].ToString()); }
+            }
+            return Rooms;
+        }
+
+        public static List<JToken> Messages(string ChannelID,int Limit=10)
+        {
+            List<JToken> MessageList = new List<JToken> { };
+            JToken Res = NetworkInterface.Request("channels/" + ChannelID + "/messages?limit=" + Limit, WToken: true, Method: "GET");
+            if (Res != null)
+            {
+                foreach (JToken Message in Res) { MessageList.Add(Message); }
+            }
+            return MessageList;
+        }
+    }
+}
---
+++ DiscordWalker/Backend/Walker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using DiscordWalker.Backend.Discord;
+using Newtonsoft.Json.Linq;
+
+namespace DiscordWalker.Backend
+{
+    public static class Walker
+    {
+        public static List<String> StartWalking(string StartInviteCode, int MaxDiscordsWalked=10, int JoinDelay=5)
+        {
+            List<String> WalkedGuilds = new List<string> { },
+                WalkedCodes = new List<string> { },
+                UnWalkedCodes = WalkDiscord(StartInviteCode, JoinDelay, ref WalkedGuilds);
+            for (int i = 0; i < MaxDiscordsWalked && i < UnWalkedCodes.Count; i++)
+            {
+                List<String> NewCodes = WalkDiscord(UnWalkedCodes.First(), JoinDelay, ref WalkedGuilds);
+                if (NewCodes != null)
+                {
+                    WalkedCodes.Add(UnWalkedCodes.First());
+                    NewCodes.ForEach(delegate (String S) { if (!WalkedCodes.Contains(S) && !UnWalkedCodes.Contains(S)) { UnWalkedCodes.Add(S); } });
+                }
+                UnWalkedCodes.RemoveAt(0);
+            }
+            return WalkedCodes;
+        }
+
+        static List<String> WalkDiscord(string InviteCode, int JoinDelay, ref List<String> WalkedGuilds)
+        {
+            string GuildID = Actions.JoinServer(InviteCode);
+            if (GuildID == null) { return null; }
+            if (WalkedGuilds.Contains(GuildID)) { return null; }
+            WalkedGuilds.Add(GuildID);
+
+            Console.WriteLine("Joined Guild: " + GuildID + " And Found These Valid Codes: ");
+
+            List<String> ValidCodes = new List<string> { };
+            foreach (string ChannelID in Fetch.TextChannels(GuildID))
+            {
+                foreach (JToken Message in Fetch.Messages(ChannelID, 100).Where(x => x["content"].ToString().Contains("https://discord.gg/")))
+                {
+                    string Code = ExtractInviteCode(Message["content"].ToString());
+                    if (Actions.ServerExists(Code)) { ValidCodes.Add(Code); Console.Write(Code + ","); }
+                }
+            }
+            if (ValidCodes.Count == 0) { Console.WriteLine("No Valid Codes"); }
+            Console.WriteLine("\nWaiting "+JoinDelay+" Seconds before Continuing\n");
+            Thread.Sleep(JoinDelay * 1000);
+            return ValidCodes;
+        }
+
+        static string ExtractInviteCode(string Message)
+        {
+            string Code = "";
+            foreach (Char C in Message.Split(new string[] { "https://discord.gg/" }, StringSplitOptions.None)[1])
+            {
+                if (Char.IsWhiteSpace(C)) { break; }
+                Code += C;
+            }
+            return Code;
+        }
+    }
+}
